380499ffb2e3e1c4ea369c9c783dae12995632eb
Facundo Gerstner <facugerstner_29@hotmail.com>
2022-12-26 19:15:18 -0300

    Circleci project setup (#1)
    * Add .circleci/config.yml
    
    * setup
    
    * add dev workflow
    
    * add install job
    
    * fixes
    
    * add requires
    
    * change workspace attachment
    
    * format config file
    
    * add badge
    
    * format

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,27 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
-  },
-  extends: ['eslint:recommended', 'standard-with-typescript', 'prettier'],
-  parser: '@typescript-eslint/parser',
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx'],
-      extends: [
-        'plugin:@typescript-eslint/recommended',
-        'plugin:@typescript-eslint/recommended-requiring-type-checking',
-      ],
-      parserOptions: {
-        project: ['./tsconfig.json'], // Specify it only for TypeScript files
-      },
+    env: {
+        browser: true,
+        es2021: true,
     },
-  ],
-  plugins: ['@typescript-eslint'],
-  root: true,
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  rules: {
-  }
+    extends: ['eslint:recommended', 'standard-with-typescript', 'prettier'],
+    parser: '@typescript-eslint/parser',
+    overrides: [
+        {
+            files: ['*.ts', '*.tsx'],
+            extends: [
+                'plugin:@typescript-eslint/recommended',
+                'plugin:@typescript-eslint/recommended-requiring-type-checking',
+            ],
+            parserOptions: {
+                project: ['./tsconfig.json'], // Specify it only for TypeScript files
+            },
+        },
+    ],
+    plugins: ['@typescript-eslint'],
+    root: true,
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    rules: {},
 }
