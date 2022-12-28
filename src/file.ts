6f73217887a765b3a386f8e0406e7cc00776ded2
Facundo Gerstner <facugerstner_29@hotmail.com>
2022-12-28 19:56:46 -0300

    normalize file paths

--- src/file.ts
+++ src/file.ts
@@ -1,9 +1,11 @@
 import { readFileSync } from 'fs';
 import md5 from 'js-md5';
+import path from 'path';
 
 const linesCount = (filePath: string): number =>
     source(filePath).split('\n').length;
-const source = (filePath: string): string => readFileSync(filePath, 'utf-8');
+const source = (filePath: string): string =>
+    readFileSync(path.normalize(filePath), 'utf-8');
 const md5Digest = (filePath: string): string =>
     md5.digest(source(filePath)).toString();
 
