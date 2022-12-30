ec12a09e85b6d0671e6881f42aaa77991e9febea
Facundo Gerstner <facugerstner_29@hotmail.com>
2022-12-30 12:40:26 -0300

    update tests

--- test/coveralls.spec.ts
+++ test/coveralls.spec.ts
@@ -1,20 +1,41 @@
 import Coveralls from '../src/coveralls';
 import Environment from '../src/environment';
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
+import FormData from 'form-data';
 
 jest.mock('axios');
 jest.mock('js-md5', () => ({
     digest: jest.fn().mockReturnValue('sourceDigest'),
 }));
+
 describe('Coveralls', () => {
     let coveralls: Coveralls;
+    const axiosMock = axios as unknown as jest.Mock;
 
     beforeEach(() => {
         coveralls = new Coveralls();
     });
 
+    const itMakesRequestWithCorrectParams = (): void =>
+        it('makes request to coveralls with correct params', async () => {
+            await coveralls.submitFromLcov(
+                require.resolve('./fixtures/repo/coverage/lcov.info')
+            );
+
+            const calledData = (
+                axiosMock.mock.calls[0] as object[]
+            )[0] as AxiosRequestConfig;
+
+            expect(
+                (calledData.data as FormData)
+                    .getBuffer()
+                    .toString()
+                    .match(/{.*}/)
+            ).toMatchSnapshot();
+        });
+
     describe('submitFromLcov', () => {
-        describe('with circleci environment', () => {
+        describe('with PR circleci environment', () => {
             beforeEach(() => {
                 Environment.CIRCLECI = true;
                 Environment.CIRCLE_BUILD_NUM = '1';
@@ -23,18 +44,15 @@ describe('Coveralls', () => {
                 Environment.CIRCLE_PULL_REQUEST_ID = '123';
             });
 
-            it('makes request to coveralls with correct params', async () => {
-                await coveralls.submitFromLcov(
-                    require.resolve('./fixtures/repo/coverage/lcov.info')
-                );
+            itMakesRequestWithCorrectParams();
+        });
 
-                expect(axios).toHaveBeenCalledWith(
-                    expect.objectContaining({
-                        method: 'POST',
-                        url: 'https://coveralls.io/api/v1/jobs',
-                    })
-                );
+        describe('with no PR circleci environment', () => {
+            beforeEach(() => {
+                Environment.CIRCLE_PULL_REQUEST_ID = '';
             });
+
+            itMakesRequestWithCorrectParams();
         });
     });
 });
