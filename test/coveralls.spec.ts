ace7d51959676dd114ba14a8e4d7ca73c7b09482
Facundo Gerstner <facugerstner_29@hotmail.com>
2022-12-30 11:44:59 -0300

    Revert "remove test"
    This reverts commit 6d6173e7f638008c53605a38f8d0929cd215569d.

---
+++ test/coveralls.spec.ts
@@ -0,0 +1,40 @@
+import Coveralls from '../src/coveralls';
+import Environment from '../src/environment';
+import axios from 'axios';
+
+jest.mock('axios');
+jest.mock('js-md5', () => ({
+    digest: jest.fn().mockReturnValue('sourceDigest'),
+}));
+describe('Coveralls', () => {
+    let coveralls: Coveralls;
+
+    beforeEach(() => {
+        coveralls = new Coveralls();
+    });
+
+    describe('submitFromLcov', () => {
+        describe('with circleci environment', () => {
+            beforeEach(() => {
+                Environment.CIRCLECI = true;
+                Environment.CIRCLE_BUILD_NUM = '1';
+                Environment.CIRCLE_COMMIT_SHA =
+                    '0d15c65a92b7c3642883a528da8dd4841accde21';
+                Environment.CIRCLE_PULL_REQUEST_ID = '123';
+            });
+
+            it('makes request to coveralls with correct params', async () => {
+                await coveralls.submitFromLcov(
+                    require.resolve('./fixtures/repo/coverage/lcov.info')
+                );
+
+                expect(axios).toHaveBeenCalledWith(
+                    expect.objectContaining({
+                        method: 'POST',
+                        url: 'https://coveralls.io/api/v1/jobs',
+                    })
+                );
+            });
+        });
+    });
+});
